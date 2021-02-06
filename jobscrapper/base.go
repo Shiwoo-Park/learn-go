4361dbd5df51f3337a4262f52973041345c5576e
silva.park <silva.park@sendbird.com>
2021-02-06 22:32:46 +0900

    learned more about Go

--- jobscrapper/base.go
+++ jobscrapper/base.go
@@ -1,25 +1,48 @@
 package jobscrapper
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/shiwoo-park/learngo/modules"
 )
 
+type JobInfo struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 var baseURL = "https://kr.indeed.com/jobs?q=%EA%B0%9C%EB%B0%9C%EC%9E%90&l=%ED%8C%90%EA%B5%90"
 
-func visitPage(page int) {
-	pageURL := baseURL + "&start=" + strconv.Itoa(page)
+func cleanString(s string) string {
+	txtArray := strings.Fields(strings.TrimSpace(s))
+	return strings.Join(txtArray, " ")
+}
 
+func visitPage(page int, c chan *JobInfo) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*10)
+
+	fmt.Println("visitPage:", pageURL)
 	res, err := http.Get(pageURL)
 	defer res.Body.Close()
 	modules.HandlelErrorForResponse(res, err)
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	modules.HandlelError(err)
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		// fmt.Println(s.Html())
+	doc.Find("#pageContent .jobsearch-SerpJobCard").Each(func(i int, s *goquery.Selection) {
+		attrVal, _ := s.Attr("data-jk")
+		jobID := attrVal
+		title := cleanString(s.Find(".title>a").Text())
+		location := cleanString(s.Find(".sjcl").Text())
+		salary := cleanString(s.Find(".salaryText").Text())
+		summary := cleanString(s.Find(".summary").Text())
+		jobInfo := JobInfo{id: jobID, title: title, location: location, salary: salary, summary: summary}
+		c <- &jobInfo
 	})
 }
 
@@ -34,12 +57,19 @@ func ExampleScrape() {
 	modules.HandlelError(err)
 
 	// Get total page number
-	pages := 0
+	pageCount := 0
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		// fmt.Println(s.Html())
-		pages = s.Find("a").Length()
+		pageCount = s.Find("a").Length()
 	})
 
-	for 
+	var c = make(chan *JobInfo)
+	for i := 0; i < pageCount; i++ {
+		go visitPage(i, c)
+	}
+
+	for i := 0; i < pageCount*10; i++ {
+		fmt.Printf("%+v\n\n", <-c)
+	}
 
 }
